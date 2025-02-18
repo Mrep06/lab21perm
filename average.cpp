3025cbc234dea5b75a8354bb9bc5591986210ef5
Perm <wannuwatbuayangyuen@gmail.com>
2025-02-18 15:30:24 +0700

    average completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc,char *argv[]){
+int sum = 0;
+int count = 0;
+    for(int i = 1; i < argc;i++){
+        sum += atoi(argv[i]);
+        count++;
+    }
+    cout <<"---------------------------------" << endl;
+    cout <<"Average of "<< count << " numbers = "<< sum/double(count) << endl;
+    cout <<"---------------------------------" << endl;
+return 0;
+}
